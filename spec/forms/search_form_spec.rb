b793b2e522f8ce64843832b1c7fe8de87e974156
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-19 11:11:13 +0000

    Adds basic validation for postcode/geocode
    This purposefully avoids using `presence` and `format` as there'll be
    another form of validation (geocoding the given postcode) shortly.

--- spec/forms/search_form_spec.rb
+++ spec/forms/search_form_spec.rb
@@ -1,5 +1,9 @@
 RSpec.describe SearchForm do
   describe 'validation' do
+    it 'is valid with valid attributes' do
+      expect(described_class.new(postcode: 'RG2 1AA')).to be_valid
+    end
+
     it 'requires a postcode' do
       expect(described_class.new).to_not be_valid
     end
