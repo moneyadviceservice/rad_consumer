4ff94cb12eb7b671bd3397d2091ac4571e3a6108
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-02-19 10:19:31 +0000

    Refactor classnames into variables

--- app/assets/javascripts/modules/SearchFilter.js
+++ app/assets/javascripts/modules/SearchFilter.js
@@ -2,30 +2,36 @@ define(['jquery'], function($) {
 
   'use strict';
 
-  var $element = $('[data-search-filter]'),
+  var $elements = $('[data-search-filter]'),
       $trigger = $('[data-search-filter-trigger]'),
       $triggerIcon = $('[data-search-filter-icon]'),
       $triggerHeading = $('[data-search-heading-second]'),
+      $triggerHeadingIconOpen = 'search-filter__triangle-icon--down',
+      $triggerHeadingSecondStyle = 'search-filter__heading--second',
+      $statusHidden = 'is-hidden',
       $target = $('[data-search-filter-target]');
 
-  $element.each(function() {
-    var $this = $(this);
+  $elements.each(function(indexInArray, objectInArray) {
+    var $elementTarget = $(objectInArray).find($target),
+        $elementTrigger = $(objectInArray).find($trigger),
+        $elementTriggerIcon = $(objectInArray).find($triggerIcon),
+        $elementTriggerHeading = $(objectInArray).find($triggerHeading);
 
-    $this.find($trigger).on('click', function(event) {
+    $elementTrigger.on('click', function(event) {
       event.preventDefault();
 
       // hide all of the filter sections on the page
-      $target.addClass('is-hidden');
+      $target.addClass($statusHidden);
 
       // force the icons to revert to pointing left/closed
-      $triggerIcon.removeClass('search__triangle-icon--down');
+      $triggerIcon.removeClass($triggerHeadingIconOpen);
 
       // find the specific filter section and toggle its visibility and set the icon to open/down
-      $this.find($target).toggleClass('is-hidden');
-      $this.find($triggerIcon).toggleClass('search__triangle-icon--down');
+      $elementTarget.toggleClass($statusHidden);
+      $elementTriggerIcon.toggleClass($triggerHeadingIconOpen);
 
       // if data-attribute is present then set the correct border-radius
-      $this.find($triggerHeading).length ? $this.find($triggerHeading).removeClass('search-filter__heading--second') : $triggerHeading.addClass('search-filter__heading--second');
+      $elementTriggerHeading.length ? $elementTriggerHeading.removeClass($triggerHeadingSecondStyle) : $triggerHeading.addClass($triggerHeadingSecondStyle);
     });
   });
 });
