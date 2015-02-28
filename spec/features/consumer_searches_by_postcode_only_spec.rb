1ba604cb29d9588da417d35cc8bfa8e53e26b303
Ben Lovell <benjamin.lovell@gmail.com>
2015-02-28 14:23:54 +0000

    Moves indexing operations into helpers
    These will be a good candidate for sharing, once we know how else we'll
    be consuming them.

--- spec/features/consumer_searches_by_postcode_only_spec.rb
+++ spec/features/consumer_searches_by_postcode_only_spec.rb
@@ -17,19 +17,13 @@ RSpec.feature 'Consumer searches by postcode only' do
   end
 
   scenario 'Consumer enters a valid postcode' do
-    VCR.turned_off do
-      begin
-        WebMock.allow_net_connect!
-
-        given_i_am_on_the_rad_landing_page
-        and_firms_with_advisers_covering_my_postcode_were_previously_indexed
-        when_i_search_with_a_reading_postcode
-        then_i_am_shown_firms_with_advisers_covering_my_postcode
-        and_i_am_not_shown_non_postcode_searchable_firms
-        and_the_firms_are_ordered_by_distance_in_miles_to_me
-      ensure
-        WebMock.disable_net_connect!
-      end
+    with_elastic_search! do
+      given_i_am_on_the_rad_landing_page
+      and_firms_with_advisers_covering_my_postcode_were_previously_indexed
+      when_i_search_with_a_reading_postcode
+      then_i_am_shown_firms_with_advisers_covering_my_postcode
+      and_i_am_not_shown_non_postcode_searchable_firms
+      and_the_firms_are_ordered_by_distance_in_miles_to_me
     end
   end
 
@@ -39,15 +33,11 @@ RSpec.feature 'Consumer searches by postcode only' do
   end
 
   def and_firms_with_advisers_covering_my_postcode_were_previously_indexed
-    # FIXME: Entirely temporary - hold on to your hats!
-    `curl -XDELETE -sS http://127.0.0.1:9200/rad_test`
-    `curl -XPOST -sS http://127.0.0.1:9200/rad_test -d @elastic_search_mapping.json`
-
-    @reading   = create(:adviser, postcode: 'RG2 8EE', latitude: 51.428473, longitude: -0.943616)
-    @leicester = create(:adviser, postcode: 'LE1 6SL', latitude: 52.633013, longitude: -1.131257)
-    @glasgow   = create(:adviser, postcode: 'G1 5QT', latitude: 55.856191, longitude: -4.247082)
-
-    `curl -XPOST -sS http://127.0.0.1:9200/rad_test/_refresh`
+    with_fresh_index! do
+      @reading   = create(:adviser, postcode: 'RG2 8EE', latitude: 51.428473, longitude: -0.943616)
+      @leicester = create(:adviser, postcode: 'LE1 6SL', latitude: 52.633013, longitude: -1.131257)
+      @glasgow   = create(:adviser, postcode: 'G1 5QT', latitude: 55.856191, longitude: -4.247082)
+    end
   end
 
   def when_i_search_with_a_reading_postcode
@@ -89,5 +79,23 @@ RSpec.feature 'Consumer searches by postcode only' do
 
   def then_i_am_told_the_postcode_is_incorrect
     expect(landing_page.in_person).to be_invalid_postcode
+  end
+
+  def with_fresh_index!
+    `curl -XDELETE -sS http://127.0.0.1:9200/rad_test`
+    `curl -XPOST -sS http://127.0.0.1:9200/rad_test -d @elastic_search_mapping.json`
+    yield
+    `curl -XPOST -sS http://127.0.0.1:9200/rad_test/_refresh`
+  end
+
+  def with_elastic_search!
+    VCR.turned_off do
+      begin
+        WebMock.allow_net_connect!
+        yield
+      ensure
+        WebMock.disable_net_connect!
+      end
+    end
   end
 end
