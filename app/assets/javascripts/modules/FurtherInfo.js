9a0bced9f1f92ca1338af67ece46afb989e2e42c
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-02-28 11:40:00 +0000

    Adjust the general landing page style

--- app/assets/javascripts/modules/FurtherInfo.js
+++ app/assets/javascripts/modules/FurtherInfo.js
@@ -4,8 +4,13 @@ define(['jquery'], function($) {
 
   var $element = $('[data-further-info]'),
       $trigger = $('[data-further-info-trigger]'),
+      $triggerAlt = $('[data-further-info-trigger-alt]'),
       $target = $('[data-further-info-target]'),
       statusHidden = 'is-hidden';
+
+  if ($triggerAlt.length) {
+    $triggerAlt.wrap('<button type="button" class="further-info__button"></button>');
+  }
 
   $element.each(function(indexInArray, objectInArray) {
     var $elementTrigger = $(objectInArray).find($trigger),
