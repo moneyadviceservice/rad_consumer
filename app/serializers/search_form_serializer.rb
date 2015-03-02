3489d2cfcf3701b9bf6af8458dfa5ef6d08a2871
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-03-02 15:10:54 +0000

    Includes filters in the query built from the search form.
    The existing serializer spec was more hassle than good, so it was removed.

--- app/serializers/search_form_serializer.rb
+++ app/serializers/search_form_serializer.rb
@@ -15,22 +15,37 @@ class SearchFormSerializer < ActiveModel::Serializer
 
   def query
     {
-      'bool' => {
-        'must': [
-          { 'match': { 'postcode_searchable': true } },
-          {
-            'nested' => {
-              'path' => 'advisers',
-              'filter' => {
-                'geo_distance' => {
-                  'distance' => '650miles',
-                  'location' => object.coordinates.reverse
-                }
-              }
-            }
-          }
-        ]
+      'bool': {
+        'must': (build_postcode_filters + build_types_of_advice_filters)
       }
     }
+  end
+
+  private
+
+  def build_postcode_filters
+    [
+      {'match': {'postcode_searchable': true }},
+      {
+        'nested' => {
+          'path' => 'advisers',
+          'filter' => {
+            'geo_distance' => {
+              'distance' => '650miles',
+              'location' => object.coordinates.reverse
+            }
+          }
+        }
+      }
+    ]
+  end
+
+  def build_types_of_advice_filters
+    [].tap do |filters|
+      filters << { match: { 'options_when_paying_for_care': true } } if object.options_when_paying_for_care == '1'
+      filters << { match: { 'equity_release': true } } if object.equity_release == '1'
+      filters << { match: { 'inheritance_tax_planning': true } } if object.inheritance_tax_planning == '1'
+      filters << { match: { 'wills_and_probate': true } } if object.wills_and_probate == '1'
+    end
   end
 end
