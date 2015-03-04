14196fcd39edc62f808a3c88112085712d101006
Ben Lovell <benjamin.lovell@gmail.com>
2015-03-04 17:22:43 +0000

    Includes fanout of geocode/index jobs from core
    Required a slight change to trigger indexing of factory data. Also took
    the opportunity to break out the individual elasticsearch helper
    actions since `with_fresh_index!` was becoming unwieldy.

--- spec/support/elastic_search_helper.rb
+++ spec/support/elastic_search_helper.rb
@@ -1,8 +1,17 @@
 module ElasticSearchHelper
   def with_fresh_index!
+    rebuild_index!
+    yield
+    index_all!
+    refresh_index!
+  end
+
+  def rebuild_index!
     `curl -XDELETE -sS http://127.0.0.1:9200/rad_test`
     `curl -XPOST -sS http://127.0.0.1:9200/rad_test -d @elastic_search_mapping.json`
-    yield
+  end
+
+  def refresh_index!
     `curl -XPOST -sS http://127.0.0.1:9200/rad_test/_refresh`
     `sleep 3` if ENV['TRAVIS']
   end
@@ -16,5 +25,9 @@ module ElasticSearchHelper
         WebMock.disable_net_connect!
       end
     end
+  end
+
+  def index_all!
+    Firm.all.map { |f| IndexFirmJob.perform_later(f) }
   end
 end
