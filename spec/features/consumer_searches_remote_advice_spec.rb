8476d29318fd382eef1546edfd818d80ee023468
Sebastian Nanek <snanek@gmail.com>
2015-03-06 13:47:30 +0000

    Spec refactorings and improvements

--- spec/features/consumer_searches_remote_advice_spec.rb
+++ spec/features/consumer_searches_remote_advice_spec.rb
@@ -2,15 +2,15 @@ RSpec.feature 'Consumer searches for phone or online advice' do
   let(:landing_page)        { LandingPage.new }
   let(:remote_results_page) { RemoteResultsPage.new }
 
-  let(:online_advice) { create :other_advice_method, name: "Advice online (e.g. by video call / conference / email)" }
-  let(:phone_advice)  { create :other_advice_method, name: "Advice by telephone" }
+  let(:online_advice) { create(:other_advice_method, name: 'Advice online (e.g. by video call / conference / email)') }
+  let(:phone_advice)  { create(:other_advice_method, name: 'Advice by telephone') }
 
   scenario 'Selecting online advice' do
     with_elastic_search! do
       given_i_am_on_the_rad_landing_page
       and_firms_providing_remote_services_were_previously_indexed
       when_i_submit_a_search_selecting_online_advice
-      then_i_am_shown_firms_and_subs_that_provide_advice_online
+      then_i_am_shown_firms_that_provide_advice_online
       and_they_are_ordered_alphabetically
     end
   end
@@ -18,10 +18,10 @@ RSpec.feature 'Consumer searches for phone or online advice' do
 
   def and_firms_providing_remote_services_were_previously_indexed
     with_fresh_index! do
-      @online_only      = create(:firm, other_advice_methods: [ online_advice ])
-      @online_and_phone = create(:firm, other_advice_methods: [ online_advice, phone_advice ])
-      @phone_only       = create(:firm, other_advice_methods: [ phone_advice ])
-      @only_in_person   = create(:firm, other_advice_methods: [])
+      @online_only      = create(:firm, registered_name: 'The End Advisory',       other_advice_methods: [ online_advice ])
+      @online_and_phone = create(:firm, registered_name: 'Remoteley Advisory',     other_advice_methods: [ online_advice, phone_advice ])
+      @phone_only       = create(:firm, registered_name: 'Cold Callers Limited',   other_advice_methods: [ phone_advice ])
+      @only_in_person   = create(:firm, registered_name: 'ACME Retirement Advice', other_advice_methods: [])
     end
   end
 
@@ -36,14 +36,14 @@ RSpec.feature 'Consumer searches for phone or online advice' do
     end
   end
 
-  def then_i_am_shown_firms_and_subs_that_provide_advice_online
+  def then_i_am_shown_firms_that_provide_advice_online
     expect(remote_results_page.firms).to be_present
   end
 
   def and_they_are_ordered_alphabetically
-    expect(remote_results_page.firms.map(&:name)).to contain_exactly(
+    expect(remote_results_page.firms.map(&:name)).to eql([
+      @online_and_phone.registered_name,
       @online_only.registered_name,
-      @online_and_phone.registered_name
-    )
+    ])
   end
 end
--- spec/support/remote_results_page.rb
+++ spec/support/remote_results_page.rb
@@ -1,3 +1,5 @@
+require_relative 'firm_section'
+
 class RemoteResultsPage < SitePrism::Page
   set_url '/en/remote-help-search'
   set_url_matcher %r{/(en|cy)/remote-help-search}
