8df40c62fb9b9b7e93f0f71ee0062dbb4ed5f9ae
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-03-06 10:55:24 +0000

    Add eduation module

--- app/assets/javascripts/modules/FurtherInfo.js
+++ app/assets/javascripts/modules/FurtherInfo.js
@@ -5,16 +5,24 @@ define(['jquery'], function($) {
   var $element = $('[data-further-info]'),
       $trigger = $('[data-further-info-trigger]'),
       $triggerAlt = $('[data-further-info-trigger-alt]'),
+      $triggerIcon = $('[data-further-info-icon]'),
       $target = $('[data-further-info-target]'),
-      statusHidden = 'is-hidden';
+      statusHidden = 'is-hidden',
+      iconOpen = 'has-icon--open',
+      iconClosed = 'has-icon--closed';
 
   if ($triggerAlt.length) {
     $triggerAlt.wrap('<button type="button" class="further-info__button"></button>');
   }
 
+  if ($triggerIcon.length) {
+    $triggerIcon.addClass(iconClosed);
+  }
+
   $element.each(function(indexInArray, objectInArray) {
     var $elementTrigger = $(objectInArray).find($trigger),
-        $elementTarget = $(objectInArray).find($target);
+        $elementTarget = $(objectInArray).find($target),
+        $elementIcon = $(objectInArray).find($triggerIcon);
 
     $elementTrigger.on('click', function(event) {
       event.preventDefault();
@@ -25,6 +33,15 @@ define(['jquery'], function($) {
       } else {
         $elementTarget.addClass(statusHidden);
       }
+
+      if ($elementIcon.hasClass(iconClosed)) {
+        $elementIcon.removeClass(iconClosed);
+        $elementIcon.addClass(iconOpen);
+      } else {
+        $elementIcon.removeClass(iconOpen);
+        $elementIcon.addClass(iconClosed);
+      }
+
     });
   });
 });
