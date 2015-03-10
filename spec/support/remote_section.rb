4d702e44f8f298e49ac6dd72e3c595b59ed52c5a
Sebastian Nanek <snanek@gmail.com>
2015-03-10 15:11:35 +0000

    Error message when no remote advice method selected

--- spec/support/remote_section.rb
+++ spec/support/remote_section.rb
@@ -2,4 +2,8 @@ class RemoteSection < SitePrism::Section
   element :by_phone, '.t-advice-method-1'
   element :online, '.t-advice-method-2'
   element :search, '.button--primary'
+
+  def invalid_advice_methods?
+    has_css?('.field_with_errors .t-advice_methods')
+  end
 end
