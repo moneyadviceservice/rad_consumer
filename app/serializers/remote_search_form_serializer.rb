0f20ca887cba25614e73ccd61a3649be7b6cdb4c
Sebastian Nanek <snanek@gmail.com>
2015-03-11 16:13:32 +0000

    Support for filtering by advice type

--- app/serializers/remote_search_form_serializer.rb
+++ app/serializers/remote_search_form_serializer.rb
@@ -10,12 +10,32 @@ class RemoteSearchFormSerializer < ActiveModel::Serializer
   def query
     {
       'filtered': {
-        'filter': {
-          'in': {
-            'other_advice_methods': object.remote_advice_method_ids
+        'filter': [
+          {
+            'in': {
+              'other_advice_methods': object.remote_advice_method_ids
+            }
+          }
+        ]
+      }
+    }.tap do |query|
+      if types_of_advice?
+        query[:filtered][:filter] << {
+          'script': {
+            'script': types_of_advice_filter_expression
           }
         }
-      }
-    }
+      end
+    end
+  end
+
+  private
+
+  def types_of_advice_filter_expression
+    object.types_of_advice.map { |field| "doc['#{field}'].value > 0" }.join(' && ')
+  end
+
+  def types_of_advice?
+    object.types_of_advice.present?
   end
 end
--- spec/support/remote_section.rb
+++ spec/support/remote_section.rb
@@ -3,6 +3,11 @@ class RemoteSection < SitePrism::Section
   element :online, '.t-advice-method-2'
   element :search, '.button--primary'
 
+  element :options_when_paying_for_care, '#remote_search_form_options_when_paying_for_care'
+  element :equity_release, '#remote_search_form_equity_release'
+  element :inheritance_tax_planning, '#remote_search_form_inheritance_tax_planning'
+  element :wills_and_probate, '#remote_search_form_wills_and_probate'
+
   def invalid_advice_methods?
     has_css?('.field_with_errors .t-advice_methods')
   end
