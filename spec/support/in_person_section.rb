21b473ddfee7433f236881491d00059b323a1676
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-03-12 13:13:34 +0000

    Adds pension pot filter elements to the in-person page object section.

--- spec/support/in_person_section.rb
+++ spec/support/in_person_section.rb
@@ -2,6 +2,9 @@ class InPersonSection < SitePrism::Section
   element :postcode, '.t-postcode'
   element :search, '.button--primary'
 
+  element :pension_pot, '#search_form_pension_pot'
+  element :pension_pot_size, '#search_form_pension_pot_size'
+
   element :options_when_paying_for_care, '#search_form_options_when_paying_for_care'
   element :equity_release, '#search_form_equity_release'
   element :inheritance_tax_planning, '#search_form_inheritance_tax_planning'
