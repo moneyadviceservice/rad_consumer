ca20086d5f15d23c989264d24454d1a7543511ba
Ben Lovell <benjamin.lovell@gmail.com>
2015-03-16 16:25:17 +0000

    Completes basic case for pension pot filter

--- spec/features/consumer_filters_by_investment_size_spec.rb
+++ spec/features/consumer_filters_by_investment_size_spec.rb
@@ -6,8 +6,6 @@ RSpec.feature 'Consumer filters by pension pot size' do
   let(:longitude) { rand(-4.247082..-0.943616) }
 
   scenario 'Consumer selects the pension pot size filter' do
-    pending 'WIP'
-
     with_elastic_search! do
       given_i_am_on_the_rad_landing_page
       and_firms_with_advisers_were_previously_indexed
@@ -55,14 +53,11 @@ RSpec.feature 'Consumer filters by pension pot size' do
 
   def and_firms_with_advisers_were_previously_indexed
     with_fresh_index! do
-      @small_pot_size_firm = create(:firm_with_no_business_split, pension_transfer_percent: 90, other_percent: 10, investment_sizes: @investment_sizes.values_at(0, 1))
-      create_list(:adviser, 1, firm: @small_pot_size_firm, latitude: latitude, longitude: longitude)
+      @small_pot_size_firm = create(:firm_with_no_business_split, retirement_income_products_percent: 90, other_percent: 10, investment_sizes: @investment_sizes.values_at(0, 1))
+      create(:adviser, firm: @small_pot_size_firm, latitude: latitude, longitude: longitude)
 
-      @pot_transfers_firm = create(:firm_with_no_business_split, pension_transfer_percent: 100, investment_sizes: @investment_sizes.values_at(4))
-      create_list(:adviser, 2, firm: @pot_transfers_firm, latitude: latitude, longitude: longitude)
-
-      @large_pot_size_firm = create(:firm_with_no_business_split, pension_transfer_percent: 50, other_percent: 50, investment_sizes: @investment_sizes.values_at(2, 3))
-      create_list(:adviser, 3, firm: @large_pot_size_firm, latitude: latitude, longitude: longitude)
+      @excluded = create(:firm_with_no_business_split, retirement_income_products_percent: 100)
+      create(:adviser, firm: @excluded, latitude: latitude, longitude: longitude)
     end
   end
 
