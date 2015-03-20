a1bf79ba71e4ff9f3c7ac985f62885a8ccb56285
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-03-20 14:05:37 +0200

    Adjust the functionality of the search filter on the landing page

--- app/assets/javascripts/modules/SearchFilter.js
+++ app/assets/javascripts/modules/SearchFilter.js
@@ -9,15 +9,22 @@ define(['jquery'], function($) {
       $triggerHeading = $('[data-search-filter-heading]'),
       $triggerHeadingSecond = $('[data-search-heading-second]'),
 
-      // set statuses
       borderRadius = 'search-filter__heading--second',
       iconOpen = 'search-filter__triangle-icon--down',
       statusHidden = 'is-hidden',
 
       $button = $('<button type="button" class="search-filter__heading-trigger" data-search-filter-trigger></button>'),
-      $target = $('[data-search-filter-target]');
+      $target = $('[data-search-filter-target]'),
+      $targetClosed = $('[data-search-filter-target="closed"]');
 
   $triggerHeading.wrap($button);
+
+  if ($targetClosed.find('.form__row').hasClass('is-errored') === true) {
+    $target.addClass(statusHidden);
+    $targetClosed.removeClass(statusHidden);
+  } else {
+    $targetClosed.addClass(statusHidden);
+  }
 
   $elements.each(function(indexInArray, objectInArray) {
     var $elementTarget = $(objectInArray).find($target),
@@ -27,22 +34,10 @@ define(['jquery'], function($) {
 
     $elementTrigger.on('click', function(event) {
       $target.addClass(statusHidden);
-
-      // force the icons to revert to pointing left/closed
       $triggerIcon.removeClass(iconOpen);
 
-      // find the specific filter section and toggle its visibility,
-      // and set the icon to open/down
       $elementTarget.toggleClass(statusHidden);
       $elementTriggerIcon.toggleClass(iconOpen);
-
-      // if data-attribute is present then set the correct border-radius
-      if ( $elementTriggerHeading.length ) {
-        $elementTriggerHeading.removeClass(borderRadius);
-      } else {
-        $triggerHeadingSecond.addClass(borderRadius);
-      }
-
     });
   });
 });
