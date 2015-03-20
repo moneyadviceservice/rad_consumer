aee2e317f66b3fcbab43db4e40d594a54a1cd2f4
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-03-20 17:08:33 +0200

    Extract filter into a partial

--- spec/support/in_person_section.rb
+++ spec/support/in_person_section.rb
@@ -2,14 +2,14 @@ class InPersonSection < SitePrism::Section
   element :postcode, '.t-postcode'
   element :search, '.button--primary'
 
-  element :pension_pot, '#in_person_search_form_retirement_income_products'
-  element :pension_pot_size, '#in_person_search_form_pension_pot_size'
-  element :pension_pot_transfer, '#in_person_search_form_pension_transfer'
+  element :pension_pot, '.t-retirement-income-products'
+  element :pension_pot_size, '.t-pension-pot-size'
+  element :pension_pot_transfer, '.t-pension-transfer'
 
-  element :options_when_paying_for_care, '#in_person_search_form_options_when_paying_for_care'
-  element :equity_release, '#in_person_search_form_equity_release'
-  element :inheritance_tax_planning, '#in_person_search_form_inheritance_tax_planning'
-  element :wills_and_probate, '#in_person_search_form_wills_and_probate'
+  element :options_when_paying_for_care, '.t-options-when-paying-for-care'
+  element :equity_release, '.t-equity-release'
+  element :inheritance_tax_planning, '.t-inheritance-tax-planning'
+  element :wills_and_probate, '.t-wills-and-probate'
 
   def invalid_postcode?
     has_css?('.field_with_errors .t-postcode')
