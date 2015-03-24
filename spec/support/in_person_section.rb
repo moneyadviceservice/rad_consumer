b0e3989275dcafbf3c1ad40b9fdae50f5fdb435a
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-03-24 12:04:36 +0000

    Use types of advice filter option keys for testing classes.

--- spec/support/in_person_section.rb
+++ spec/support/in_person_section.rb
@@ -6,10 +6,10 @@ class InPersonSection < SitePrism::Section
   element :pension_pot_size, '.t-pension-pot-size'
   element :pension_pot_transfer, '.t-pension-transfer'
 
-  element :options_when_paying_for_care, '.t-options-when-paying-for-care'
-  element :equity_release, '.t-equity-release'
-  element :inheritance_tax_planning, '.t-inheritance-tax-planning'
-  element :wills_and_probate, '.t-wills-and-probate'
+  element :options_when_paying_for_care, '.t-options_when_paying_for_care'
+  element :equity_release, '.t-equity_release'
+  element :inheritance_tax_planning, '.t-inheritance_tax_planning'
+  element :wills_and_probate, '.t-wills_and_probate'
 
   def invalid_postcode?
     has_css?('.field_with_errors .t-postcode')
