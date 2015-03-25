8187f2ebfb8d8f41f361c82abd101d6b59e2e6bb
Ben Lovell <benjamin.lovell@gmail.com>
2015-03-25 13:17:15 -0400

    Hides label for no accreditations/qualifications

--- spec/features/consumer_views_a_search_result_spec.rb
+++ spec/features/consumer_views_a_search_result_spec.rb
@@ -17,6 +17,46 @@ RSpec.feature 'Consumer views a search result' do
     end
   end
 
+  scenario 'Viewing a single result with no accreditations or qualifications' do
+    with_elastic_search! do
+      given_an_indexed_firm_and_associated_adviser_without_qualifications_and_accreditations
+      when_i_perform_a_basic_search
+      then_i_see_the_indexed_firm
+      and_i_do_not_see_any_qualifications_and_accreditations
+      and_i_do_not_see_the_label
+    end
+  end
+
+
+  def given_an_indexed_firm_and_associated_adviser_without_qualifications_and_accreditations
+    with_fresh_index! do
+      @principal = create(:principal, website_address: 'http://www.example.com')
+
+      @firm = create(:firm_with_no_business_split,
+        principal: @principal,
+        free_initial_meeting: true,
+        minimum_fixed_fee: 1000,
+        retirement_income_products_percent: 10,
+        pension_transfer_percent: 10,
+        long_term_care_percent: 10,
+        equity_release_percent: 10,
+        inheritance_tax_and_estate_planning_percent: 10,
+        wills_and_probate_percent: 10,
+        other_percent: 40,
+        in_person_advice_methods: [1, 2].map { |i| create(:in_person_advice_method, order: i) },
+        other_advice_methods: [1, 2].map { |i| create(:other_advice_method, order: i) },
+        investment_sizes: [1, 2].map { |i| create(:investment_size, order: i) }
+      )
+
+      create(:adviser,
+        firm: @firm,
+        latitude: 51.428473,
+        longitude: -0.943616,
+        accreditations: [],
+        qualifications: []
+      )
+    end
+  end
 
   def given_an_indexed_firm_and_associated_adviser
     with_fresh_index! do
@@ -96,5 +136,14 @@ RSpec.feature 'Consumer views a search result' do
   def and_i_see_the_qualifications_and_accreditations_the_firms_advisers_possess
     expect(@displayed_firm).to have_qualifications(count: 2)
     expect(@displayed_firm).to have_accreditations(count: 3)
+  end
+
+  def and_i_do_not_see_any_qualifications_and_accreditations
+    expect(@displayed_firm).to_not have_qualifications
+    expect(@displayed_firm).to_not have_accreditations
+  end
+
+  def and_i_do_not_see_the_label
+    expect(@displayed_firm).to_not have_qualifications_heading
   end
 end
