28a32b49edf7d0720163dac84a970ae7a252f03d
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-04-29 13:33:26 +0100

    Geocoder only fires on button press

--- app/assets/javascripts/modules/GeoLocate.js
+++ app/assets/javascripts/modules/GeoLocate.js
@@ -2,8 +2,7 @@ define(['jquery'], function($) {
 
   'use strict';
 
-  var location = $('[data-geo-location]'),
-      description = $('.search-filter__description'),
+  var description = $('.search-filter__description'),
       input = $('[data-geo-location-input]'),
       label = $('[data-geo-location-label]'),
       container = $('[data-geo-location-container]'),
@@ -15,10 +14,6 @@ define(['jquery'], function($) {
     geocoder = new google.maps.Geocoder();
   }
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-  }
-
   function successFunction(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
@@ -27,35 +22,27 @@ define(['jquery'], function($) {
   }
 
   function errorFunction() {
-    alert("Geocoder failed");
+    alert("Geocoder failed - we cannot determine your location");
   }
 
   function codeLatLng(lat, lng) {
     var latlng = new google.maps.LatLng(lat, lng);
 
     geocoder.geocode({'latLng' : latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
+      if (status === google.maps.GeocoderStatus.OK) {
         if (results[1]) {
           var arrAddress = results;
 
           $.each(arrAddress, function(i, address_component) {
-            if (address_component.types[0] == "postal_code") {
+            if (address_component.types[0] === "postal_code") {
               postcode = address_component.address_components[0].long_name;
 
-              location.text(postcode);
-              input.attr('placeholder', postcode);
+              container.text(postcode);
+              description.remove();
+              button.remove();
 
-              button.click(function(){
-                event.preventDefault();
-
-                container.text(postcode);
-                description.remove();
-                button.remove();
-
-                input.val(lat + " " + lng).addClass('visually-hidden');
-
-                label.text('Search with postcode');
-              });
+              input.val(lat + " " + lng).addClass('visually-hidden');
+              label.text('Search with postcode');
             }
           });
         } else {
@@ -68,4 +55,14 @@ define(['jquery'], function($) {
   }
 
   initialise();
+
+  button.removeClass('is-hidden');
+
+  button.click(function(){
+    event.preventDefault();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+    }
+  });
 });
