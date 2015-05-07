8435dfb272d7c73f13088d2ac0dffaea4e5f751d
Caden Lovelace <caden@herostrat.us>
2015-05-07 16:47:02 +0100

    Make mobile-specific interface apply/unapply on resize.

--- app/assets/javascripts/modules/ResultsModule.js
+++ app/assets/javascripts/modules/ResultsModule.js
@@ -9,9 +9,48 @@ define(['jquery'], function($) {
       $buttonElement = $('<button type="button" class="education__button unstyled-button" data-results-module-trigger></button>'),
       $iconElement = $('<span class="education__icon education__icon--plus" data-results-module-icon></span>'),
       statusHidden = 'is-hidden-on-mobile',
-      $targetClosed = $('[data-results-module-target="closed"]');
+      $targetClosed = $('[data-results-module-target="closed"]'),
+      iconSelector = '[data-results-module-icon]',
+      triggerSelector = '[data-results-module-trigger]',
+      mobileApplied = false;
 
-  if ($(window).width() <= 720) {
+  function bindResultsModuleEventHandlers() {
+    $element.each(function(_, resultsModule) {
+      $(resultsModule).on('click', triggerSelector, function() {
+        var $elementTrigger = $(resultsModule).find(triggerSelector),
+            $elementTriggerIcon = $(resultsModule).find(iconSelector),
+            $elementTarget = $(resultsModule).find($target),
+            $icons = $(iconSelector);
+
+        $icons.removeClass('education__icon--minus');
+        $icons.addClass('education__icon--plus');
+
+        if ($elementTarget.hasClass(statusHidden)) {
+          $target.addClass(statusHidden);
+          $elementTriggerIcon.removeClass('education__icon--plus');
+          $elementTriggerIcon.addClass('education__icon--minus');
+          $elementTarget.removeClass(statusHidden);
+        } else {
+          $elementTarget.addClass(statusHidden);
+          $elementTriggerIcon.addClass('education__icon--plus');
+          $elementTriggerIcon.removeClass('education__icon--minus');
+        }
+      });
+    });
+  }
+
+  function applyMobileElementsIfWindowSizeMatches() {
+    var windowWidth = $(window).width();
+    if (mobileApplied === false && windowWidth <= 720) {
+      mobileApplied = true;
+      addMobileElements();
+    } else if (mobileApplied === true && windowWidth > 720) {
+      mobileApplied = false;
+      removeMobileElements();
+    }
+  }
+
+  function addMobileElements() {
     $heading.wrap($buttonElement);
     $heading.prepend($iconElement);
 
@@ -23,28 +62,19 @@ define(['jquery'], function($) {
     }
   }
 
-  var $icon = $('[data-results-module-icon]'),
-      $button = $('[data-results-module-trigger]');
+  function removeMobileElements() {
+    $heading.unwrap();
+    $heading.find('.education__icon').remove();
 
-  $element.each(function(indexInArray, objectInArray) {
-    var $elementTrigger = $(objectInArray).find($button),
-        $elementTriggerIcon = $(objectInArray).find($icon),
-        $elementTarget = $(objectInArray).find($target);
+    if ($targetClosed.find('.form__row').hasClass('is-errored') === true) {
+      $target.removeClass(statusHidden);
+      $targetClosed.addClass(statusHidden);
+    } else {
+      $targetClosed.removeClass(statusHidden);
+    }
+  }
 
-    $elementTrigger.click(function() {
-      $icon.removeClass('education__icon--minus');
-      $icon.addClass('education__icon--plus');
-
-      if ($elementTarget.hasClass(statusHidden)) {
-        $target.addClass(statusHidden);
-        $elementTriggerIcon.removeClass('education__icon--plus');
-        $elementTriggerIcon.addClass('education__icon--minus');
-        $elementTarget.removeClass(statusHidden);
-      } else {
-        $elementTarget.addClass(statusHidden);
-        $elementTriggerIcon.addClass('education__icon--plus');
-        $elementTriggerIcon.removeClass('education__icon--minus');
-      }
-    });
-  });
+  bindResultsModuleEventHandlers();
+  applyMobileElementsIfWindowSizeMatches();
+  $(window).bind('orientationchange resize', applyMobileElementsIfWindowSizeMatches);
 });
