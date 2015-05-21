07d5cab8cc61a8c67e45575cfc69fbfa7b195544
Dan Munckton <dan@cultivatehq.com>
2015-05-21 11:06:45 +0100

    Create a helper for constructing VCR options for feature tests

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,7 +1,12 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.configure_rspec_metadata!
 
   # Allow access to Elastic Search running locally
   config.ignore_localhost = true
+end
+
+def vcr_options_for_feature(cassette_name)
+  { cassette_name: cassette_name, record: :new_episodes }
 end
