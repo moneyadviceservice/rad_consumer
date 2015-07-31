c2d03a1f0ca20d3883cda60518156e4f9540c658
Dan Munckton <dan@cultivatehq.com>
2015-07-31 16:26:24 +0100

    Remote only searches now exclude any firms with in person advice methods checked

--- spec/features/consumer_searches_remote_advice_spec.rb
+++ spec/features/consumer_searches_remote_advice_spec.rb
@@ -3,6 +3,8 @@ RSpec.feature 'Consumer searches for phone or online advice',
   let(:landing_page) { LandingPage.new }
   let(:remote_results_page) { RemoteResultsPage.new }
 
+  let!(:in_person_advice_methods) { create_list(:in_person_advice_method, 3) }
+
   let!(:phone_advice)  { create(:other_advice_method, name: 'Telephone', order: 1) }
   let!(:online_advice) { create(:other_advice_method, name: 'Online', order: 2) }
 
@@ -58,23 +60,34 @@ RSpec.feature 'Consumer searches for phone or online advice',
     end
   end
 
-
   def and_firms_providing_remote_services_were_previously_indexed
     with_fresh_index! do
-      @online_only = create(:firm, registered_name: 'The End Advisory', other_advice_methods: [online_advice])
-      @online_and_phone = create(:firm, registered_name: 'Remoteley Advisory', other_advice_methods: [online_advice, phone_advice])
-      @phone_only = create(:firm, registered_name: 'Cold Callers Limited', other_advice_methods: [phone_advice])
-      @only_in_person = create(:firm, registered_name: 'ACME Retirement Advice', other_advice_methods: [])
+      # Remote options only
+      @online_only = create(:firm, registered_name: 'The End Advisory', in_person_advice_methods: [], other_advice_methods: [online_advice])
+      @online_and_phone = create(:firm, registered_name: 'Remoteley Advisory', in_person_advice_methods: [], other_advice_methods: [online_advice, phone_advice])
+      @phone_only = create(:firm, registered_name: 'Cold Callers Limited', in_person_advice_methods: [], other_advice_methods: [phone_advice])
+
+      # Face to face options only
+      @only_in_person = create(:firm, registered_name: 'ACME Retirement Advice', in_person_advice_methods: in_person_advice_methods, other_advice_methods: [])
+
+      # Face to face but some of the optional remote options checked too
+      @in_person_and_also_remote = create(:firm, registered_name: 'ACME Retirement Advice', in_person_advice_methods: in_person_advice_methods, other_advice_methods: [online_advice, phone_advice])
     end
   end
 
   def and_firms_providing_various_types_of_remote_services_were_indexed
     with_fresh_index! do
-      @equity = create(:firm_with_no_business_split, registered_name: 'Equity release advisory', pension_transfer_flag: true, other_advice_methods: [online_advice, phone_advice])
-      @wills = create(:firm_with_no_business_split, registered_name: 'Wills advisory', equity_release_flag: true, other_flag: true, other_advice_methods: [online_advice, phone_advice])
-      @probate = create(:firm_with_no_business_split, registered_name: 'Probate advisory', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, other_advice_methods: [online_advice, phone_advice])
-      @wills_and_equity = create(:firm_with_no_business_split, registered_name: 'Paying for care and equity advisory', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, other_advice_methods: [online_advice, phone_advice])
-      @offline_and_wills = create(:firm_with_no_business_split, registered_name: 'Wills Offliney Advisory Ltd', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, other_advice_methods: [])
+      # Remote options only
+      @equity = create(:firm_with_no_business_split, registered_name: 'Equity release advisory', pension_transfer_flag: true, in_person_advice_methods: [], other_advice_methods: [online_advice, phone_advice])
+      @wills = create(:firm_with_no_business_split, registered_name: 'Wills advisory', equity_release_flag: true, other_flag: true, in_person_advice_methods: [], other_advice_methods: [online_advice, phone_advice])
+      @probate = create(:firm_with_no_business_split, registered_name: 'Probate advisory', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, in_person_advice_methods: [], other_advice_methods: [online_advice, phone_advice])
+      @wills_and_equity = create(:firm_with_no_business_split, registered_name: 'Paying for care and equity advisory', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, in_person_advice_methods: [], other_advice_methods: [online_advice, phone_advice])
+
+      # Face to face options only
+      @in_person_and_wills = create(:firm_with_no_business_split, registered_name: 'Wills Face to Face Advice Ltd', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, in_person_advice_methods: in_person_advice_methods, other_advice_methods: [])
+
+      # Face to face but some of the optional remote options checked too
+      @in_person_and_also_remote = create(:firm_with_no_business_split, registered_name: 'Wills Face to Face or Phone Advice Ltd', equity_release_flag: true, wills_and_probate_flag: true, other_flag: true, in_person_advice_methods: in_person_advice_methods, other_advice_methods: [online_advice, phone_advice])
     end
   end
 
@@ -169,7 +182,7 @@ RSpec.feature 'Consumer searches for phone or online advice',
   end
 
   def and_the_list_does_not_include_offline_only_advisories
-    expect(remote_results_page.firm_names).not_to include(@offline_and_wills.registered_name)
+    expect(remote_results_page.firm_names).not_to include(@in_person_and_wills.registered_name)
   end
 
   def and_i_am_not_shown_the_advisers_distance
