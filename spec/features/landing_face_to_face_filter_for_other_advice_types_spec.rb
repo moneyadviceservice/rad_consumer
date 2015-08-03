bf9c4b9b4bb19623357d62892b0c6a10f2ddea24
Dan Munckton <dan@cultivatehq.com>
2015-08-03 15:08:17 +0100

    Enhance existing face-to-face search feature to ensure remote results are excluded

--- spec/features/landing_face_to_face_filter_for_other_advice_types_spec.rb
+++ spec/features/landing_face_to_face_filter_for_other_advice_types_spec.rb
@@ -12,11 +12,13 @@ RSpec.feature 'Landing page, consumer requires advice on various topics in perso
       and_i_indicate_i_need_advice_on_various_topics
       when_i_submit_the_face_to_face_advice_search
       then_i_am_shown_firms_that_provide_the_selected_types_of_advice
+      and_i_am_not_shown_firms_that_provide_remote_only_advice
       and_they_are_ordered_by_location
     end
   end
 
   def given_firms_with_advisers_were_previously_indexed
+
     with_fresh_index! do
       @first= create(:firm_with_no_business_split, retirement_income_products_flag: true, wills_and_probate_flag: true, other_flag: true)
       @leicester = create(:adviser, firm: @first, latitude: 52.633013, longitude: -1.131257)
@@ -29,6 +31,19 @@ RSpec.feature 'Landing page, consumer requires advice on various topics in perso
 
       create(:firm_with_no_business_split, other_flag: true) do |f|
         create(:adviser, firm: f, latitude: 51.428473, longitude: -0.943616)
+      end
+
+      other_advice_methods = [
+        create(:other_advice_method, name: 'Phone', order: 1),
+        create(:other_advice_method, name: 'Online', order: 2)
+      ]
+      @remote_advice_only = create(:firm_with_no_business_split,
+                                   retirement_income_products_flag: true,
+                                   wills_and_probate_flag: true,
+                                   other_flag: true,
+                                   in_person_advice_methods: [],
+                                   other_advice_methods: other_advice_methods) do |f|
+        create(:adviser, firm: f, latitude: 55.856191, longitude: -4.247082)
       end
     end
   end
@@ -59,6 +74,10 @@ RSpec.feature 'Landing page, consumer requires advice on various topics in perso
   def then_i_am_shown_firms_that_provide_the_selected_types_of_advice
     expect(results_page).to be_displayed
     expect(results_page).to have_firms(count: 2)
+  end
+
+  def and_i_am_not_shown_firms_that_provide_remote_only_advice
+    expect(results_page.firm_names).not_to include(@remote_advice_only.registered_name)
   end
 
   def and_they_are_ordered_by_location
