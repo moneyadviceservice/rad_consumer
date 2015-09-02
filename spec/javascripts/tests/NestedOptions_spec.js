cdd2f99855a110120eb5d0fa4895a456ba52968b
Caden Lovelace <caden@herostrat.us>
2015-09-02 12:11:56 +0100

    Reform NestedOptions into require module so it can be tested.
    Previously it wasn't being re-run for each test, which meant the tests
    failed. They didn't previously fail because they were set up in a way
    that caused false positives as long as there weren't too many tests. But
    when we added some more tests, we got those failures dropping through
    into ours.

--- spec/javascripts/tests/NestedOptions_spec.js
+++ spec/javascripts/tests/NestedOptions_spec.js
@@ -1,73 +1,38 @@
 describe('show hidden options on checkbox change', function () {
-
   'use strict';
 
+  var $html, $nestedOptionsComponent, $nestedOptionsTrigger, $nestedOptions;
+
   beforeEach(function(done){
-    this.$html = $(window.__html__['spec/javascripts/fixtures/NestedOptions.html']).appendTo('body');
-    this.$nestedOptionsComponent = $('[data-nested-options-component]');
-    this.$nestedOptionsTrigger = $('[data-nested-options-trigger]');
-    this.$nestedOptions = $('[data-nested-options]');
-    done();
+    requirejs(['NestedOptions'], function (NestedOptions) {
+      $html = $(window.__html__['spec/javascripts/fixtures/NestedOptions.html']).appendTo('body');
+      $nestedOptionsComponent = $('[data-nested-options-component]');
+      $nestedOptionsTrigger = $('[data-nested-options-trigger]');
+      $nestedOptions = $('[data-nested-options]');
+      new NestedOptions();
+      done();
+    });
   });
 
   afterEach(function() {
-    this.$html.remove();
-  });
-
-  describe('NestedOptions is not loaded', function(){
-    it('page elements exist', function(done) {
-      expect(this.$nestedOptionsComponent).to.exist;
-      expect(this.$nestedOptions).to.exist;
-      expect(this.$nestedOptionsTrigger).to.exist;
-      done();
-    });
-
-    it('is hidden on default', function(done) {
-      expect(this.$nestedOptions).to.have.class('is-hidden');
-      done();
-    });
-
-    it('hidden options are not shown when checkbox is checked', function(done) {
-      this.$nestedOptionsTrigger.trigger('click');
-      expect(this.$nestedOptionsTrigger).to.be.checked;
-      expect(this.$nestedOptions).to.have.class('is-hidden');
-      done();
-    });
+    $html.remove();
   });
 
   describe('NestedOptions is loaded', function() {
-    it('page elements exist', function(done) {
-      var self = this;
-
-      requirejs(['NestedOptions'], function (NestedOptions) {
-        expect(self.$nestedOptionsComponent).to.exist;
-        expect(self.$nestedOptions).to.exist;
-        expect(self.$nestedOptionsTrigger).to.exist;
-      }, done);
-
-      done();
+    it('page elements exist', function() {
+      expect($nestedOptionsComponent).to.exist;
+      expect($nestedOptions).to.exist;
+      expect($nestedOptionsTrigger).to.exist;
     });
 
-    it('is hidden on default', function(done) {
-      var self = this;
-
-      requirejs(['NestedOptions'], function (NestedOptions) {
-        expect(self.$nestedOptions).to.have.class('is-hidden');
-      }, done);
-
-      done();
+    it('is hidden on default', function() {
+      expect($nestedOptions).to.have.class('is-hidden');
     });
 
-    it('hidden options are shown on checkbox change', function(done) {
-      var self = this;
-
-      requirejs(['NestedOptions'], function (NestedOptions) {
-        self.$nestedOptionsTrigger.trigger('click');
-        expect(self.$nestedOptionsTrigger).to.be.checked;
-        expect(self.$nestedOptions).to.not.have.class('is-hidden');
-      }, done);
-
-      done();
+    it('hidden options are shown on checkbox change', function() {
+      $nestedOptionsTrigger.trigger('click');
+      expect($nestedOptionsTrigger).to.be.checked;
+      expect($nestedOptions).to.not.have.class('is-hidden');
     });
   });
 });
