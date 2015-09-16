6167a78746a2dcce9027e0022000ced0fb146548
Alan Gardner <urfolomeus@gmail.com>
2015-09-16 15:55:04 +0100

    Restyling the results summary header

--- spec/support/results_page.rb
+++ spec/support/results_page.rb
@@ -10,9 +10,7 @@ class ResultsPage < SitePrism::Page
   section :criteria, SearchCriteriaSection, '.t-search-result-form .t-criteria'
   sections :firms, FirmSection, 'li.t-firm'
 
-  element :first_record, '.t-first-record'
-  element :last_record, '.t-last-record'
-  element :total_records, '.t-total-records'
+  element :summary, '.t-results-summary-text'
 
   element :errors, '.l-landing-page__validation'
   element :incorrect_criteria_message, '.t-incorrect-criteria'
@@ -22,19 +20,7 @@ class ResultsPage < SitePrism::Page
   end
 
   def showing_firms?(first, to:, of:)
-    first?(first) && last?(to) && total?(of)
-  end
-
-  def first?(n)
-    first_record.text == n.to_s
-  end
-
-  def last?(n)
-    last_record.text == n.to_s
-  end
-
-  def total?(n)
-    total_records.text == n.to_s
+    summary.text == "Showing #{first}-#{to} of #{of} nearest advisers"
   end
 
   def firm_names
