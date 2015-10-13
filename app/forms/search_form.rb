37ffe5770d0d4f2e08b8672e37825bbfb2b3f9e7
Dan Munckton <dan@cultivatehq.com>
2015-10-13 16:45:11 +0100

    Make helpers to extract the ids for selected filters

--- app/forms/search_form.rb
+++ app/forms/search_form.rb
@@ -77,6 +77,14 @@ class SearchForm
     end
   end
 
+  def selected_qualification_id
+    selected_filter_id_for(Qualification)
+  end
+
+  def selected_accreditation_id
+    selected_filter_id_for(Accreditation)
+  end
+
   def postcode
     @postcode if face_to_face?
   end
@@ -96,6 +104,10 @@ class SearchForm
     postcode.try(:upcase!)
   end
 
+  def prefix_for(model)
+    model.model_name.singular[0]
+  end
+
   def filters_for(model)
     key_to_i = -> (k, v) { [k.to_s.to_i, v] }
     I18n.t("search.filter.#{model.model_name.i18n_key}.ordinal").map(&key_to_i).to_h
@@ -103,10 +115,22 @@ class SearchForm
 
   def options_for(model)
     filters = filters_for(model)
-    prefix = model.model_name.singular[0]
     model
       .where(order: filters.keys)
       .pluck(:order, :id)
-      .map { |order, id| [filters[order], "#{prefix}#{id}"] }
+      .map { |order, id| [filters[order], "#{prefix_for(model)}#{id}"] }
+  end
+
+  def selected_filter_id_for(model)
+    is_desired_type = ->(prefix, item) { !!item[/^#{prefix}/] }
+    extract_id = ->(item) { item[1..-1] }
+    type_prefix = prefix_for(model)
+
+    [qualifications_and_accreditations]
+      .compact
+      .select(&is_desired_type.curry[type_prefix])
+      .map(&extract_id)
+      .map(&:to_i)
+      .first
   end
 end
