a9704bbdca79915827eaf474251c519e6e55e69e
Mark Connell <mark@markconnell.co.uk>
2015-10-15 11:19:21 +0100

    Remove guard that was preventing the drop down from working properly unless 'pension pot' checkbox was set

--- spec/serializers/search_form_serializer_spec.rb
+++ spec/serializers/search_form_serializer_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe SearchFormSerializer do
-  let(:params) { {} }
+  let(:default_params) { { pension_pot_size: SearchForm::ANY_SIZE_VALUE } }
   let(:search_form) { SearchForm.new(params) }
 
   subject { described_class.new(search_form) }
 
   describe '#query' do
     context 'no firm_id is included in the search form params' do
-      let(:params) { { firm_id: nil } }
+      let(:params) { default_params.merge(firm_id: nil) }
 
       it 'includes a term filter to limit the query to the one firm id' do
         query_hash = subject.query[:filtered][:query]
@@ -15,11 +15,29 @@ RSpec.describe SearchFormSerializer do
     end
 
     context 'a firm_id is included in the search form params' do
-      let(:params) { { firm_id: 123 } }
+      let(:params) { default_params.merge(firm_id: 123) }
 
       it 'includes a term filter to limit the query to the one firm id' do
         query_hash = subject.query[:filtered][:query]
         expect(query_hash).to eq(bool: { must: [{ term: { _id: 123 } }] })
+      end
+    end
+
+    context 'the pension pot size is "any size"' do
+      let(:params) { default_params }
+
+      it 'does not include a match filter for investment sizes' do
+        query_hash = subject.query[:filtered][:query]
+        expect(query_hash).to eq(bool: { must: [] })
+      end
+    end
+
+    context 'the pension pot size is set to a value' do
+      let(:params) { default_params.merge(pension_pot_size: '3') }
+
+      it 'includes a filter for the investment size' do
+        query_hash = subject.query[:filtered][:query]
+        expect(query_hash).to eq(bool: { must: [{ match: { investment_sizes: '3' } }] })
       end
     end
   end
