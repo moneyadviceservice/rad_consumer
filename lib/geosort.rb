cc196aaf892ca9c415b6afce57fb6fc46d08d2ae
Mark Connell <mark@markconnell.co.uk>
2015-11-19 14:34:57 +0000

    sort office locations by distance from the search

---
+++ lib/geosort.rb
@@ -0,0 +1,15 @@
+module Geosort
+  def self.by_distance(initial_location, objects_with_location)
+    obj_distance_pairs = {}
+
+    objects_with_location.each do |obj|
+      distance = Geocoder::Calculations.distance_between(initial_location.to_a, obj.location.to_a)
+      obj_distance_pairs[obj] = distance
+    end
+
+    obj_distance_pairs
+      .sort_by {|_key, value| value}
+      .flatten
+      .reject {|x| x.is_a? Float }
+  end
+end
--- spec/features/firm_profile_for_in_person_search_spec.rb
+++ spec/features/firm_profile_for_in_person_search_spec.rb
@@ -10,7 +10,8 @@ RSpec.feature 'Firm profile page from an "in-person" search',
       and_i_perform_an_in_person_search
       when_i_view_the_firm_profile
       and_i_select_the_offices_tab
-      then_i_should_see_both_offices_listed
+      then_i_should_see_a_number_of_offices_listed
+      and_they_should_be_ordered_by_closest_to_search_postcode
     end
   end
 
@@ -18,14 +19,16 @@ RSpec.feature 'Firm profile page from an "in-person" search',
     with_fresh_index! do
       @firm = create(:firm)
       create(:adviser, firm: @firm, postcode: 'RG2 8EE', latitude: 51.428473, longitude: -0.943616, travel_distance: 100)
-      create(:office, firm: @firm)
+      @firm.main_office.update_attributes!(address_line_one: '120 Holborn', address_line_two: 'London', address_town: 'London', address_county: nil, address_postcode: 'EC1N 2TD', latitude: 51.518148, longitude:-0.108013)
+      create(:office, firm: @firm, address_line_one: 'Phoenix House', address_line_two: '18 King William St', address_town: 'London', address_county: nil, address_postcode: 'EC4N 7BP', latitude: 51.511224, longitude:-0.087422)
+      create(:office, firm: @firm, address_line_one: '12 Leather Ln', address_line_two: 'London', address_town: 'London', address_county: nil, address_postcode: 'EC1N 7SS', latitude: 51.519144, longitude:-0.108927)
     end
   end
 
   def and_i_perform_an_in_person_search
     landing_page.load
     landing_page.in_person.tap do |f|
-      f.postcode.set 'RG2 9FL'
+      f.postcode.set 'EC1N 2TD'
       f.search.click
     end
 
@@ -41,7 +44,12 @@ RSpec.feature 'Firm profile page from an "in-person" search',
     profile_page.office_tab.click
   end
 
-  def then_i_should_see_both_offices_listed
-    expect(profile_page.offices.length).to eq(2)
+  def then_i_should_see_a_number_of_offices_listed
+    expect(profile_page.offices.length).to eq(3)
+  end
+
+  def and_they_should_be_ordered_by_closest_to_search_postcode
+    expect(profile_page.offices.first.postcode.text).to eq('EC1N 2TD')
+    expect(profile_page.offices.last.postcode.text).to eq('EC4N 7BP')
   end
 end
