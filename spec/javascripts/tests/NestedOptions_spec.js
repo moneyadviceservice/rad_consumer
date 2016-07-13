f435bcd1d154bff073c9cdaf213899e7ee9ce0d0
David Trussler <mail@davidtrussler.net>
2016-07-13 10:26:09 +0100

    TP: 7439, Comment: Updates failing test

--- spec/javascripts/tests/NestedOptions_spec.js
+++ spec/javascripts/tests/NestedOptions_spec.js
@@ -4,35 +4,37 @@ describe('show hidden options on checkbox change', function () {
   var $html, $nestedOptionsComponent, $nestedOptionsTrigger, $nestedOptions;
 
   beforeEach(function(done){
+    var self = this;
+
     requirejs(['NestedOptions'], function (NestedOptions) {
-      $html = $(window.__html__['spec/javascripts/fixtures/NestedOptions.html']).appendTo('body');
-      $nestedOptionsComponent = $('[data-nested-options-component]');
-      $nestedOptionsTrigger = $('[data-nested-options-trigger]');
-      $nestedOptions = $('[data-nested-options]');
+      self.$html = $(window.__html__['spec/javascripts/fixtures/NestedOptions.html']).appendTo('body');
+      self.$nestedOptionsComponent = $('[data-nested-options-component]');
+      self.$nestedOptionsTrigger = $('[data-nested-options-trigger]');
+      self.$nestedOptions = $('[data-nested-options]');
       new NestedOptions();
       done();
     });
   });
 
   afterEach(function() {
-    $html.remove();
+    this.$html.remove();
   });
 
   describe('NestedOptions is loaded', function() {
     it('page elements exist', function() {
-      expect($nestedOptionsComponent).to.exist;
-      expect($nestedOptions).to.exist;
-      expect($nestedOptionsTrigger).to.exist;
+      expect(this.$nestedOptionsComponent).to.exist;
+      expect(this.$nestedOptions).to.exist;
+      expect(this.$nestedOptionsTrigger).to.exist;
     });
 
     it('is hidden on default', function() {
-      expect($nestedOptions).to.have.class('is-hidden');
+      expect(this.$nestedOptions).to.have.class('is-hidden');
     });
 
     it('hidden options are shown on checkbox change', function() {
-      $nestedOptionsTrigger.trigger('click');
-      expect($nestedOptionsTrigger).to.be.checked;
-      expect($nestedOptions).to.not.have.class('is-hidden');
+      this.$nestedOptionsTrigger.trigger('click');
+      expect(this.$nestedOptionsTrigger).to.be.checked;
+      expect(this.$nestedOptions).to.not.have.class('is-hidden');
     });
   });
 });
