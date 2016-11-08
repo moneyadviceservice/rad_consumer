b1a587abbc647f4e5d2b661c7b6a7bb21d6d0f4a
David Trussler <mail@davidtrussler.net>
2016-11-08 15:28:20 +0000

    TP: 7733, Comment: Amend wrong URL

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -20,7 +20,7 @@ module SearchHelper
     return if key == 'ignored'
 
     info = t("search.accreditations.items.#{key}")
-    link_to "glossary/##{key}", class: "accreditation t-#{kind}" do
+    link_to glossary_path(locale: locale, anchor: key), class: "accreditation t-#{kind}" do
       image_tag "#{key}.png", alt: info[:title], class: 'accreditation__img'
     end
   end
