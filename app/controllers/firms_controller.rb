7132793e46742a6d54e69d761926343b24032e8c
Jones Agyemang <mightyj@hotmail.co.uk>
2017-01-24 17:27:50 +0000

    fixes search results error
      * fixes an error where search results was always returning the first item of
      * the results

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -3,7 +3,7 @@ class FirmsController < ApplicationController
     @search_form = SearchForm.new(params[:search_form].merge(firm_id: params[:id]))
     result = FirmRepository.new.search(@search_form.to_query)
 
-    @firm  = result.firms.first
+    @firm = result.firms.select { |firm| firm.id == params[:id].to_i }.first
     @offices = Geosort.by_distance(@search_form.coordinates, @firm.offices)
     @advisers = sort_advisers(@search_form, @firm.advisers)
 
