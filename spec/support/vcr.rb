bce27175c96919958b6f98a38763aceb4d92e239
Fernando Briano <fernando@picandocodigo.net>
2017-08-07 10:05:14 -0300

    make the same as master

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,24 +4,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
 
   # Allow access to Elastic Search running locally
-  # config.ignore_localhost = true
   config.ignore_localhost = true
-  # Allow access to Elastic Search running locally but record CMS
-  #config.ignore_request do |request|
-  #  uri = URI(request.uri)
-  #  puts "[#{uri.port == 9200}]request #{uri}"
-  #  uri.port == 9200
-  #end
-
-  #config.around_http_request do |request|
-  #  uri = URI(request.uri)
-  #  if ENV['MAS_CMS_URL'] =~ /#{uri.host}/
-  #    VCR.use_cassette("/CMS/#{request.method}#{uri.path}#{uri.query}", &request)
-  #  else
-  #    puts "request #{uri}"
-  #    request.proceed
-  #  end
-  #end
 end
 
 def vcr_options_for_feature(cassette_name)
