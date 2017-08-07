bacde9cc23ea2f2fe87c25a2635e69df1c42f134
Fernando Briano <fernando@picandocodigo.net>
2017-08-07 08:09:41 -0300

    test VRC setup in clean env

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,23 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
 
   # Allow access to Elastic Search running locally
-  config.ignore_localhost = true
+  # config.ignore_localhost = true
+  config.ignore_localhost = false
+  # Allow access to Elastic Search running locally but record CMS
+  config.ignore_request do |request|
+    uri = URI(request.uri)
+    uri.host == 'localhost' && uri.port != 3000
+  end
+
+  config.around_http_request do |request|
+    uri = URI(request.uri)
+
+    if ENV['MAS_CMS_URL'] =~ /#{uri.host}/
+      VCR.use_cassette("/CMS/#{request.method}#{uri.path}#{uri.query}", &request)
+    else
+      request.proceed
+    end
+  end
 end
 
 def vcr_options_for_feature(cassette_name)
