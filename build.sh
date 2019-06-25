7f1a5e22adb09bc9908c6a06ff89a3c225b1991b
osharpe <oliver.sharpe@moneyadviceservice.org.uk>
2019-06-25 12:00:34 +0100

    Add test & build scripts for Go

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -l
+
+set -e
+
+unset RUBYOPT
+export PATH=./bin:$PATH
+export BUNDLE_WITHOUT='test:development'
+
+version_number=${GO_PIPELINE_LABEL-0}
+revision=$( git rev-parse HEAD )
+build_date=$( date +'%Y-%m-%d %H:%M %z' )
+
+cat > public/version <<EOT
+{
+  "version":"$version_number",
+  "buildDate":"$build_date",
+  "gitRevision":"$revision"
+}
+EOT
+
+echo 'Cleaning temporary files'
+echo '----'
+rm -rf public/assets vendor/cache coverage log/* tmp/* .bundle/config
+
+echo 'Running Bundle package'
+echo '----'
+time bundle package --all
+
+echo 'Running Bower cache clean'
+echo '----'
+time bower cache clean
+
+echo 'Purging bower components'
+echo '----'
+[ -d vendor/assets/bower_components ] && rm -r vendor/assets/bower_components
+
+echo 'Running Bower update (via bowndler)'
+echo '----'
+time bowndler update --production --config.interactive=false
+
+echo 'Precompiling assets'
+echo '----'
+time RAILS_ENV=production RAILS_GROUPS=assets rake assets:precompile
+
+echo 'Creating RPM'
+echo '----'
+cd ..
+time /usr/local/rpm_builder/create-rails-rpm $artifact_name $artifact_name $version_number
