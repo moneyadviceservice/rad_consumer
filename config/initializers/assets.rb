090501a118e5ad95074587438aeafd6d131d3d8d
andreferreiradlw <ferreirandre25@gmail.com>
2019-11-14 10:00:01 +0000

    Changes assets.rb root path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
-  config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+  config.assets.paths << Rails.root.join('node_modules', '@bower_components')
 
   # Application Stylesheets
   config.assets.precompile += %w(
