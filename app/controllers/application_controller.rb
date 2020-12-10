703eae661a3d32de7b5ea04c0121e482fe0d02ba
Spencer Dixon <spencerlloyddixon@gmail.com>
2020-12-10 11:10:25 +0000

    Initial POC of partner tool layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_locale
-
   BANNER_DISMISSED_COOKIE_NAME = '_covid_banner'.freeze
   BANNER_DISMISSED_COOKIE_VALUE = 'y'.freeze
 
   include Chat
+
+  before_action :set_locale
+  before_action :set_layout
 
   def covid_banner_dismissed?
     cookies.permanent[BANNER_DISMISSED_COOKIE_NAME] != BANNER_DISMISSED_COOKIE_VALUE
@@ -38,5 +39,21 @@ class ApplicationController < ActionController::Base
       status: :not_found,
       layout: false
     }
+  end
+
+  def set_layout
+    self.class.layout parent_template
+  end
+
+  def parent_template
+    if syndicated_tool_request?
+      'syndicated/application'
+    else
+      'application'
+    end
+  end
+
+  def syndicated_tool_request?
+    !!request.headers['X-Syndicated-Tool']
   end
 end
