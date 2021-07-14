f24ac01cc5596a405cddc36ec6d3558b4366f37a
Ben Lovell <benjamin.lovell@gmail.com>
2021-07-14 16:29:58 +0100

    Legacy redirects to new AEM embeds
    When requests are made with the legacy host
    `directory.moneyadviceservice.org.uk` (or whatever is configured at the
    `LEGACY_HOST` env var) the application will redirect to the new AEM
    page, with the correct embed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
+ENGLISH_EMBED_URL = 'https://www.moneyhelper.org.uk/en/pensions-and-retirement/taking-your-pension/find-a-retirement-adviser'.freeze
+WELSH_EMBED_URL   = 'https://www.moneyhelper.org.uk/cy/pensions-and-retirement/taking-your-pension/find-a-retirement-adviser'.freeze
+
 Rails.application.routes.draw do
-  get '/' => redirect('/en')
+  # these would match legacy, non-embed URLs
+  constraints(MasRedirectConstraints.new(redirect: true)) do
+    get '/', to: redirect(ENGLISH_EMBED_URL)
 
-  scope '/:locale', locale: /en|cy/ do
-    root 'landing_page#show'
+    get '/en(*all)', to: redirect(ENGLISH_EMBED_URL)
+    get '/cy(*all)', to: redirect(WELSH_EMBED_URL)
+  end
 
-    get '/search', to: 'search#index'
-    get '/glossary', to: 'glossary#show'
+  # these will continue to be served from the embed URLs
+  constraints(MasRedirectConstraints.new(redirect: false)) do
+    get '/' => redirect('/en')
 
-    resources :firms, only: [:show]
+    scope '/:locale', locale: /en|cy/ do
+      root 'landing_page#show'
+
+      get '/search', to: 'search#index'
+      get '/glossary', to: 'glossary#show'
+
+      resources :firms, only: [:show]
+    end
   end
 end
