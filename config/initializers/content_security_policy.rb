1718aa0febe7c6bafa23a58a003471faea96765a
Ben Lovell <benjamin.lovell@gmail.com>
2025-05-11 14:54:05 +0100

    Configure CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,17 +4,37 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data, *%w[fonts.gstatic.com fonts.googleapis.com]
+  policy.img_src     :self, :https, :data, *%w[
+    i.ytimg.com
+    yt3.ggpht.com
+    www.youtube.com
+    maps.gstatic.com
+  ]
+  policy.object_src  :none
+  policy.script_src  :self, :https, :unsafe_inline, *%w[
+    assets.adobedtm.com
+    www.moneyhelper.org.uk
+    static.cloudflareinsights.com
+    www.googletagmanager.com
+    www.youtube.com
+    static.doubleclick.net
+    www.google.com
+    play.google.com
+    legacymasassets.blob.core.windows.net
+    maps.google.com
+  ]
+  policy.style_src   :self, :https, :unsafe_inline, *%w[
+    www.youtube.com
+    legacymasassets.blob.core.windows.net
+    fonts.googleapis.com
+  ]
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
